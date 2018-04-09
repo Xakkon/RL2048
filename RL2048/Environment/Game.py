7b5e12ec44f939753b11ffe64746f93b415ad434
Xakkon <hakon998@gmail.com>
2018-04-09 19:58:09 +0300

    Add part of environment

---
+++ RL2048/Environment/Game.py
@@ -0,0 +1,85 @@
+import numpy as np
+import random as rnd
+
+class Game(object):
+    def __init__(self):
+        self._countSpaces = 16
+        self._score = 0
+        self.NewGame()
+
+    def NewGame(self):
+        self.Field = np.zeros((4,4))
+        self._countSpaces = 16
+        self._score = 0
+        self._AddRandomNumber()
+        self._AddRandomNumber()
+
+    def _AddRandomNumber(self):
+        place = rnd.randint(0,self._countSpaces - 1)
+        curPlace = 0
+        wasPlaced = False
+        for i in range(4):
+            for j in range(4):
+                if(self.Field[i,j] == 0):
+                    if(curPlace == place):
+                        self.Field[i,j] = 2 if (rnd.randint(0,100) <= 90) else 4
+                        self._countSpaces -= 1
+                        wasPlaced = True
+                        break
+                    else:
+                        curPlace += 1
+            if(wasPlaced):
+                break
+
+    def Up(self):
+        prevScore = self._score
+        self.Field = self.Field.transpose()
+        self.Left()
+        self.Field = self.Field.transpose()
+        return self._Reward(prevScore)
+
+    def Down(self):
+        prevScore = self._score
+        self.Field = np.flipud(self.Field)
+        self.Up()
+        self.Field = np.flipud(self.Field)
+        return self._Reward(prevScore)
+
+    def Right(self):
+        prevScore = self._score
+        self.Field = np.fliplr(self.Field)
+        self.Left()
+        self.Field = np.fliplr(self.Field)
+        return self._Reward(prevScore)
+
+    def Left(self):
+        prevScore = self._score
+        bias = False #было ли смещение
+        for i in range(4):
+            emptyPos = 0 #индекс места куда должна быть смещена плитка
+            for j in range(4):
+                if(self.Field[i,j] != 0):
+                    #объединение двух плиток
+                    for k in range(j+1, 4):
+                        if(self.Field[i,k] == self.Field[i, j]):
+                            self.Field[i, j] = 2*self.Field[i,j]
+                            self._score += self.Field[i, j]
+                            self.Field[i, k] = 0
+                            self._countSpaces += 1
+                            bias = True
+                            break
+                        #если с первым ненулевым элементом справа нельзя объединить
+                        elif(self.Field[i,k] != 0): 
+                            break
+                    #смещение плитки в левый край
+                    if(j != emptyPos):
+                        self.Field[i, emptyPos] = self.Field[i, j]
+                        self.Field[i,j] = 0
+                        bias = True
+                    emptyPos += 1
+        if(bias):
+           self._AddRandomNumber()
+        return self._Reward(prevScore)
+
+    def _Reward(self, prevScore):
+        return self._score - prevScore
--- RL2048/RL2048.py
+++ RL2048/RL2048.py
@@ -1,1 +1,24 @@
+from Environment.Game import Game
 
+def PlayOnConsole():
+    gm = Game()
+    c = ""
+    print("w = Up, a = Left, s = Down, d = Right, n = NewGame, 0 = Exit")
+    print("Score =",gm._score)
+    print(gm.Field)
+    while c != "0":
+        c = input()
+        if(c == "w"):
+            gm.Up()
+        elif(c == "a"):
+            gm.Left()
+        elif(c == "s"):
+            gm.Down()
+        elif(c == "d"):
+            gm.Right()
+        elif(c == "n"):
+            gm.NewGame()
+        print("Score =",gm._score)
+        print(gm.Field)
+
+PlayOnConsole()
