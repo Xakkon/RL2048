4fb551a2748dfabba7c8f6fcdedce8b5f50ce72b
Xakkon <hakon998@gmail.com>
2018-05-02 18:27:37 +0300

    Edit PlayOnConsole method

--- RL2048/RL2048.py
+++ RL2048/RL2048.py
@@ -3,23 +3,16 @@ from Environment.Game import Game
 def PlayOnConsole():
     gm = Game()
     c = ""
+    moves = {'w' : gm.Up, 'a' : gm.Left, 's' : gm.Down, 'd' : gm.Right, 'n' : gm.NewGame}
     print("w = Up, a = Left, s = Down, d = Right, n = NewGame, 0 = Exit")
     print("Score =",gm._score)
     print(gm.Field)
     while c != "0":
         c = input()
-        if(c == "w"):
-            gm.Up()
-        elif(c == "a"):
-            gm.Left()
-        elif(c == "s"):
-            gm.Down()
-        elif(c == "d"):
-            gm.Right()
-        elif(c == "n"):
-            gm.NewGame()
-        print("Score =",gm._score)
-        print(gm.Field)
+        if (c in moves):
+            moves[c]()
+            print("Score =",gm._score)
+            print(gm.Field)
 
 def main():
     PlayOnConsole()
